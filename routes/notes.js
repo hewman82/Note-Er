24281f59c0cb60a16440e0f4b0601bd3ba1c85cc
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-10-09 18:33:14 -0700

    C:/Program Files/Git/api/notes POST request

--- routes/notes.js
+++ routes/notes.js
@@ -4,10 +4,39 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const readFile = util.promisify(fs.readFile);
 
+
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-    readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
   });
 
+// POST Route for a new note
+notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuidv4(),
+      };
+  
+      fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedData = JSON.parse(data);
+          parsedData.push(newNote);
+          fs.writeFile('./db/notes.json', JSON.stringify(parsedData, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${'./db/notes.json'}`)
+  );
+        }
+      })
+    } else {
+      res.err('Error in adding note');
+    }
+  });
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+const api = require('./routes/index');
 
-const PORT = process.env.port || 3001;
+const PORT = 3001;
 
 const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', api);
 
 app.use(express.static('public'));
+
 
 
 // GET Route for homepage
