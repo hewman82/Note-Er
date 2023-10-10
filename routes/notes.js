a51749e3dff924cd19ac2e2c20c27e168b9d1fd7
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-10-10 16:38:41 -0700

    notes.js comments

--- routes/notes.js
+++ routes/notes.js
@@ -39,19 +39,26 @@ notes.post('/', (req, res) => {
     }
   });
 
+// DELETE Route for notes
 notes.delete('/:id', (req, res) => {
+  // Save id of clicked note
   const selecId = req.params.id;
+  // Get data from notes db file
   fs.readFile('./db/notes.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
     } else {
+      // Save notes db data as array
       const noteList = JSON.parse(data);
+      // Loop through notes db array
       for(i = 0; i < noteList.length; i++) {
+        // Check if array item is the one that was clicked by comparing ids
         if(noteList[i].id === selecId) {
-          console.log(i);
+          // Splice array item
           noteList.splice(i, 1);
+          // Rewrite notes db file with new array
           fs.writeFile('./db/notes.json', JSON.stringify(noteList, null, 4), (err) =>
-          err ? console.error(err) : console.info(`\nData written to ${'./db/notes.json'}`)
+          err ? console.error(err) : console.info(`\nNote deleted from ${'./db/notes.json'}`)
         );
         }
       }
